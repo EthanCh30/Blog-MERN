d1b26972c52f3e9b51f19efd975d4e51d87a7f10
Ethan Chen <565306120@qq.com>
2024-03-21 18:34:09 +1030

    build vercel

--- api/index.js
+++ api/index.js
@@ -14,7 +14,16 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+// app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+//----------VERCEL
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true}
+));
+//---------------------
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
